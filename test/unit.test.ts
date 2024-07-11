11f8e36801fc2c612d4d01ad2fba4d7d42b38dbb
Ikbal Azimari Touré <azimari.toure.etu@esmt.sn>
2024-07-11 13:34:05 +0000

    tests added

---
+++ test/unit.test.ts
@@ -0,0 +1,13 @@
+import { describe, expect, it } from "bun:test";
+
+describe("true", () => {
+  it("should be true", () => {
+    expect(false).toBeTruthy();
+  });
+});
+
+describe("false", () => {
+  it("should be false", () => {
+    expect(true).toBeFalsy();
+  });
+});
