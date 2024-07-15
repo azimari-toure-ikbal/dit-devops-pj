ddda3e4476f9b1869fab7ba1478edf0cd5a0b17b
Ikbal Azimari Touré <azimari.toure.etu@esmt.sn>
2024-07-15 22:22:51 +0000

    added ping route

--- test/unit.test.ts
+++ test/unit.test.ts
@@ -2,12 +2,12 @@ import { describe, expect, it } from "bun:test";
 
 describe("true", () => {
   it("should be true", () => {
-    expect(true).toBeTruthy();
+    expect(false).toBeTruthy();
   });
 });
 
 describe("false", () => {
   it("should be false", () => {
     expect(false).toBeFalsy();
-  });
+  });
 });
